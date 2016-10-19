0de63ddec66ee2c7a426426c9c880224fc234b58
Andy Jeong <Joon@khcuui-Mac-Pro.local>
2016-10-19 22:33:47 +0900

    Check

--- index.js
+++ index.js
@@ -1,14 +1,60 @@
 var canvas = document.getElementById('mainCanvas');
 var context = canvas.getContext('2d');
 
-var Sprite = function(filename, spritename){
-    this.image = new Image();
-    this.image.src = filename;
-    this.name = spritename;
+var Sprite = function(filename, is_pattern){
+    this.image = null;
+    this.pattern = null;
+    this.TO_RADIANS = Math.PI/180;
+    
+    if(filename != undefined && filename != "" && filename != null){
+        this.image = new Image();
+        this.image.src = filename;
+    } else {
+        console.log('Unable to load sprite');
+    }
+    
+    this.draw = function(x, y, w, h){
+          if(w == undefined || h == undefined){
+              context.drawImage(this.image, x, y, this.image.width, this.image.height);
+          } else {
+              context.drawImage(this.image, x, y, w, h);
+          }
+    };
+    
+    this.drawPattern = function(x, y, w, h){
+        if(is_pattern){
+            this.pattern = context.createPattern(this.image, 'repeat');
+        } 
+         context.fillStyle = this.pattern;
+         context.fillRect(x, y, w, h);
+    };
+    
+    this.rotate = function(x, y, angle){
+        context.save();
+        context.translate(x, y);
+        context.rotate(angle * this.TO_RADIANS);
+        context.drawImage(this.image, -(this.image.width/2), -(this.image.height /2));
+        context.restore();
+    };
 };
 
-setup = function(){
-    var image01 = new Sprite("Resource/KakaoTalk_Photo_2016-10-18-22-34-04.png", "First Image");
-    console.log(image01.name + " Loaded");
-    context.drawImage(image01.image, 50, 50);
+function Initialize(){
+    context.beginPath();
+    context.fillStyle = "black";
+    context.rect(0, 0, 1280, 720);
+    context.fill();
+    
+    var WALL = "http://gamedesign.wdfiles.com/local--files/spriteart:sprite-art-101-brick-wall-i/sprite_bricks_tutorial_1.png";
+    var CRATE = "http://gamedesign.wdfiles.com/local--files/spriteart:sprite-art-101-brick-wall-i/brick_wall_tiled_perfect.png"
+    
+    var angle = 0;
+    var image = new Sprite(WALL, false);  
+    var pattern = new Sprite(CRATE, true);
+    
+    setInterval(function(){
+        image.draw(10, 400);
+        image.draw(0, 0, 1280, 50);
+        pattern.drawPattern(0, 720 - 50, 1280, 50);
+        image.rotate(80, 400, angle += 4.0);
+    }, 1);
 };
