ea64b007572d4a9db6f4132352868ab8c0bfff00
Sunwoo Jeong <wjdtjsdn6@gmail.com>
2016-10-22 03:03:46 +0900

    TerrainGen 0.2.0

--- worldgen.js
+++ worldgen.js
@@ -5,12 +5,35 @@ var cnt = 0;
 
 function drawWorld(){
     var tileMapString = "";
+    var perc = 11;
+    var height;
     
     setInterval(function(){
         while(cnt==0){
             for(var i = 0; i < Context.canvas.width; i+=8){
                 
-                var height = Math.floor(Math.random() * Context.canvas.height);
+                var heightCnt = 0;
+                
+                if(Math.ceil(Math.random() * 10) > perc){
+                    if(Math.floor(Math.random() * 2) == 0){
+                        height -= Math.ceil(Math.random() * 20);
+                    } else {
+                        height += Math.ceil(Math.random() * 20);
+                    }
+                } else {
+                    height = Math.floor(Math.random() * Context.canvas.height);
+                    if(height > Context.canvas.height / 2){
+                        height += 50;
+                    } else {
+                        height -= 50;
+                    }
+                    
+                    if(heightCnt == 0){
+                        perc = 0.1;
+                    }
+                    heightCnt++;
+                }
+                
                 var firstCnt = 0;
                 
                 for(var j = 0; j < Context.canvas.height; j+=8){
@@ -23,10 +46,8 @@ function drawWorld(){
                         if(firstCnt == 0){
                             var randomTop = Math.floor(Math.random() * 3);
                             Context.context.drawImage(tileSet[0][0, randomTop], i, j);
-                            console.log("First Added");
                         } else {
                             Context.context.drawImage(tileSet[0][0, 3], i, j);
-                            console.log("Now Else Added");
                         }
                         firstCnt++;
                     }
