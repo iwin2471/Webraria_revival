b153f5ce364c94fb3b96479cec46fac56c0c0a41
Sunwoo Jeong <wjdtjsdn6@gmail.com>
2016-10-22 02:35:10 +0900

    Terrain Gen ver01

--- worldgen.js
+++ worldgen.js
@@ -8,18 +8,32 @@ function drawWorld(){
     
     setInterval(function(){
         while(cnt==0){
-            for(var i = 0; i < Context.canvas.height; i+=8){
-                for(var j = 0; j < Context.canvas.width; j+=8){
-                    var terrainType = Math.floor(Math.random() * 2); //Dirt or Water
-                    var terrainTypeNum = Math.floor(Math.random() * 4); //Layer Number
-                    tileMapString += terrainType.toString() + "(" + terrainTypeNum + ") ";
+            for(var i = 0; i < Context.canvas.width; i+=8){
+                
+                var height = Math.floor(Math.random() * Context.canvas.height);
+                var firstCnt = 0;
+                
+                for(var j = 0; j < Context.canvas.height; j+=8){
+                    //var terrainType = Math.floor(Math.random() * 2); //Dirt or Water
+                    //var terrainTypeNum = Math.floor(Math.random() * 4); //Layer Number
+                    //tileMapString += terrainType.toString() + "(" + terrainTypeNum + ") ";
+                    //Context.context.drawImage(tileSet[0][terrainType, terrainTypeNum + (terrainType * 4)], j, i);
                     
-                    Context.context.drawImage(tileSet[0][terrainType, terrainTypeNum + (terrainType * 4)], j, i);
+                    if(j > height){
+                        if(firstCnt == 0){
+                            Context.context.drawImage(tileSet[0][0, 1], i, j);
+                            console.log("First Added");
+                        } else {
+                            Context.context.drawImage(tileSet[0][0, 3], i, j);
+                            console.log("Now Else Added");
+                        }
+                        firstCnt++;
+                    }
             }
             tileMapString += "\n";
         }
         console.log("World Loading Complete");
-        console.log(tileMapString);
+        //console.log(tileMapString);
         cnt++;
         }
     }, 1);
